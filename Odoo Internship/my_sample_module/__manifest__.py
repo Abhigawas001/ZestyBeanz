aedff99f463eea594c1ae0a1a56a5cdf2d3af3c6
abhishek <abhishekgawas001@gmail.com>
2025-03-26 10:51:42 +0530

    Module structure and custom model

---
+++ Odoo Internship/my_sample_module/__manifest__.py
@@ -0,0 +1,23 @@
+{
+	'name': 'Sample Module',
+	'version': '18.0.0.0',
+	'summary': 'This Module for training purpose',
+	'description': """This Module for training purpose.
+	""",
+	'category':'',
+	'author': 'Abhishek Gawas',
+	'website': 'www.zbeanztech.com',
+	"license": "LGPL-3",
+	'depends': [],
+	'data': [
+		        'security/ir.model.access.csv',
+		        'views/model_one_view.xml',
+		        'views/menu.xml',
+		],
+	'test': [],
+	'demo': [],
+	'installable': True,
+	'autoinstall': False,
+	'application': False,
+
+}
